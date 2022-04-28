690bf672daa22b9fc15153a9bf24b8e8178239ad
Cole-Narfason <48136863+Cole-Narfason@users.noreply.github.com>
2022-04-28 16:23:20 -0600

    Update deploytest.py

--- deploytest.py
+++ deploytest.py
@@ -1,20 +1,11 @@
-# import module
 import streamlit as st
-import pandas as pd
-import numpy as np
 from streamlit_keplergl import keplergl_static
-#import keplergl
 from keplergl import KeplerGl
 
- 
-# Title
-st.title("Streamlit Deploy Test")
+st.write("This is a kepler.gl map in streamlit")
 
-st.header('Choose Area of Interest')
-
-Lat = st.number_input("Please enter AOI Latitude")
-Long = st.number_input("Please enter AOI Longitude")
-
+map_1 = KeplerGl(height=400)
+keplergl_static(map_1)
 
 
 
